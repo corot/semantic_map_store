82ceb205de55143a5ed09b574fcbff07f26d1ecd
Jihoon Lee <jihoonlee.in@gmail.com>
2014-10-10 19:33:51 +0900

    make save marker and save table working

--- world_canvas_server/scripts/save_tables.py
+++ world_canvas_server/scripts/save_tables.py
@@ -15,7 +15,7 @@ from world_canvas_msgs.msg import Annotation, AnnotationData
 from world_canvas_utils.serialization import *
 
 
-def read(file):
+def read(file, world, map_name):
     yaml_data = None 
     with open(file) as f:
        yaml_data = yaml.load(f)
@@ -31,8 +31,8 @@ def read(file):
         ann.world = world
         ann.name = t['name']
         ann.type = 'yocs_msgs/Table'
-        for i in range(0, random.randint(0,11)):
-            ann.keywords.append('kw'+str(random.randint(1,11)))
+        ann.keywords.append(str(world))
+        ann.keywords.append(str(map_name))
         if 'prev_id' in vars():
             ann.relationships.append(prev_id)
         prev_id = ann.id
@@ -44,9 +44,9 @@ def read(file):
         ann.size.x = float(t['radius'])*2
         ann.size.y = float(t['radius'])*2
         ann.size.z = float(t['height'])
-        ann.pose.header.frame_id = t['frame_id']
+        ann.pose.header.frame_id = t['pose']['header']['frame_id']
         ann.pose.header.stamp = rospy.Time.now()
-        ann.pose.pose.pose = message_converter.convert_dictionary_to_ros_message('geometry_msgs/Pose',t['pose'])
+        ann.pose.pose.pose = message_converter.convert_dictionary_to_ros_message('geometry_msgs/Pose',t['pose']['pose']['pose'])
         
         # tables are assumed to lay on the floor, so z coordinate is zero;
         # but WCF assumes that the annotation pose is the center of the object
@@ -58,9 +58,9 @@ def read(file):
         object.name = t['name']
         object.radius = float(t['radius'])
         object.height = float(t['height'])
-        object.pose.header.frame_id = t['frame_id']
+        object.pose.header.frame_id = t['pose']['header']['frame_id']
         object.pose.header.stamp = rospy.Time.now()
-        object.pose.pose.pose = message_converter.convert_dictionary_to_ros_message('geometry_msgs/Pose',t['pose'])
+        object.pose.pose.pose = message_converter.convert_dictionary_to_ros_message('geometry_msgs/Pose',t['pose']['pose']['pose'])
         data = AnnotationData()
         data.id = ann.data_id
         data.type = ann.type
@@ -74,7 +74,8 @@ if __name__ == '__main__':
     rospy.init_node('tables_saver')
     world = rospy.get_param('~world')
     file  = rospy.get_param('~file')
-    anns, data = read(file)
+    map_name = rospy.get_param('~map_name')
+    anns, data = read(file, world, map_name)
 
     rospy.loginfo("Waiting for save_annotations_data service...")
     rospy.wait_for_service('save_annotations_data')
